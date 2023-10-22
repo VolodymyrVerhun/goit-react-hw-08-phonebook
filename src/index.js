284a13862ad103aa17c7c0f4c115d3622659708a
volodymyr1v <volodumur.verhyn@gmail.com>
2023-10-22 18:11:58 +0300

    4

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate loading={null} persistor={persistedStore}>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </Provider>
